4f55ef7de3ce05dc54f9f382da38d60393bd5a75
AjeeshSS <ajeeshss2019@gmail.com>
2022-12-15 17:59:13 +0530

    second

--- service.js
+++ service.js
@@ -14,11 +14,7 @@ hamburger.onclick=function(){
     navbar.classList.toggle("active");
 }
 
-
-
-
-
-
+// navbar colour---------------------
 $(document).ready(function(){
     $(".navlist").on({
         mouseenter: function(){
@@ -30,27 +26,60 @@ $(document).ready(function(){
         },
        
     }); 
-    $('.btn').click(function(){
-        var mail=document.getElementById("#email").value
+    //--------------validation----------------------------
+    $('#sub').click(function(){  
+        //email validation
+        var mail=$("#email").val()
         var chtr = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(chtr.text(mail)){
+        if(chtr.test(mail)){
             alert("valid emailId")
-            return true
+            // return true
         }
         else{
             alert("invalid emailid")
-            return false
+            // return false
         }
+        // name validation
+        var n=$("#text").val()
+        var regName = /^[a-zA-Z]+$/;
+        if(!regName.test(n)){
+            alert('Invalid name given.');
+        }else{
+            alert('Valid name given.');
+        }
+
     })
+})
+//clr btn----------------------------------------------------
+
+let btnClear=document.querySelector('#clr')
+let inputs=document.querySelectorAll('input')
+
+btnClear.addEventListener('click',()=>{
+    inputs.forEach(input => input.value = '')
 })
 
 
-// function Validate(mail) 
-// {
-//  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value))
-//   {
-//     return (true)
-//   }
-//     alert("You have entered an invalid email address!")
-//     return (false)
-// }
+// Get the scroll to top button-----------------------------------
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 700px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+
